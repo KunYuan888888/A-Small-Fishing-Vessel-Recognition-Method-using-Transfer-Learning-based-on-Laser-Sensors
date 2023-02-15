ed3d161e3aea7f83960766f8db771211128ec82a
KunYuan888888 <109008605+KunYuan888888@users.noreply.github.com>
2023-02-15 20:33:20 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# some training parameters
+#  training parameters
 EPOCHS = 200
 BATCH_SIZE = 32
 NUM_CLASSES = 4
@@ -9,4 +9,3 @@ model_dir ="/VGG-16 pre-training.h5"
 train_dir = 'E:\YK\data\\train'
 valid_dir = 'E:\YK\data\\valid'
 test_dir = 'E:\YK\data\\test'
-# test_image_path = ""
