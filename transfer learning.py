dca199b387f1ca28ed5c0e01d924d4fb82e04228
KunYuan888888 <109008605+KunYuan888888@users.noreply.github.com>
2023-02-15 20:29:31 +0800

    Update transfer learning.py

--- transfer learning.py
+++ transfer learning.py
@@ -4,7 +4,7 @@ from config import *
 import numpy as np
 import os
 from keras.models import Sequential
-from models import a_convnets, alexnet, resnet, vgg16, vgg19
+from models import  vgg16
 import time
 from prepare_data import get_datasets
 from models.vgg16 import VGG16
@@ -12,13 +12,13 @@ from models.a_convnets import A_ConvNets
 from PIL import Image
 import cv2
 
-# 判断是否使用gpu进行训练
+# Judge whether to use gpu for training
 gpu_ok = tf.test.is_gpu_available()
 print("tf_version:", tf.__version__)
 print("use GPU", gpu_ok)
 
 # train_generator, valid_generator, train_num, valid_num = get_datasets()
-# 加载模型，引入模型的参数
+# Load model and import model parameters
 best_model = A_ConvNets()
 
 # input = tf.random.uniform(shape = (1,224,224,3),dtype = tf.float32)
@@ -26,38 +26,28 @@ best_model = A_ConvNets()
 # best_model=DenseNet(img_rows=image_height,img_cols=image_width,color_type=channels,num_classes=NUM_CLASSES)
 best_model.load_weights('VGG-16 pre-training.h5')
 
-# 加载带参数的模型
+# Load model with parameters
 # best_model = tf.keras.models.load_model('VGG-16 pre-training.h5')
 
-# 复用原模型的前若干层
+# Reuse the first several layers of the original model
 l_layer = len(best_model.layers)
 print(l_layer)
 best_model.summary()
 
-# new_model = Sequential(best_model.layers[0:l_layer-6])
-# new_model = Sequential(best_model.layers[:])
-# new_model.trainable = False
-# 构造新的输出层
-# new_model.add(tf.keras.layers.Dense(units=4096, activation=tf.keras.activations.relu))
-# new_model.add(tf.keras.layers.Dropout(rate=0.5))
-# new_model.add(tf.keras.layers.Dense(units=4096, activation=tf.keras.activations.relu))
-# new_model.add(tf.keras.layers.Dropout(rate=0.5))
-# new_model.add(tf.keras.layers.Dense(units=10, activation=tf.keras.activations.softmax))
-# new_model.summary()
-# 控制前面层不动
+
 for i in range(l_layer - 3):
     best_model.layers[i].trainable = False
-# # compile模型
-# new_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001,beta_1 = 0.9,beta_2 = 0.99,decay=0.00001),
-#                   loss=tf.keras.losses.categorical_crossentropy,
-#                   metrics=['accuracy'])
-# boundaries = [50, 100, 200, 300]
-# values = [0.001, 0.0005, 0.0001, 0.00005, 0.00001]
-# piece_wise_constant_decay = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
-#     boundaries=boundaries, values=values, name=None)
-best_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.0001),
+# compile model
+new_model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001,beta_1 = 0.95,beta_2 = 0.99,decay=0.00001),
                   loss=tf.keras.losses.categorical_crossentropy,
                   metrics=['accuracy'])
+boundaries = [50, 100, 200, 300]
+values = [0.001, 0.0005, 0.0001, 0.00005, 0.00001]
+piece_wise_constant_decay = tf.keras.optimizers.schedules.PiecewiseConstantDecay(
+    boundaries=boundaries, values=values, name=None)
+# best_model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.0001),
+#                   loss=tf.keras.losses.categorical_crossentropy,
+#                   metrics=['accuracy'])
 time0 = time.time()
 
 tl_train_dir = "D:/Dataset/train"
