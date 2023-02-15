c66a2748d1e141e23a015680590d51dea26dddd4
KunYuan888888 <109008605+KunYuan888888@users.noreply.github.com>
2023-02-15 19:32:01 +0800

    Update MTF Encoding.py

--- MTF Encoding.py
+++ MTF Encoding.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pyts.image import MarkovTransitionField
 for i in range(1000):
- data=pd.read_csv("D:\Small Fishing Vessel Recognition\Original Data\Small Fishing\A%i.csv"%(i+1),header=None) #Cyclically read data
+ data=pd.read_csv("D:\Small Fishing Vessel Recognition\Original Data\PE plastic vesselsA%i.csv"%(i+1),header=None) #Cyclically read data
  X =data.values.tolist()                          # MTF transformation
  mtf = MarkovTransitionField(image_size=10)
  X_mtf = mtf.fit_transform(X)                     # Show the image for the first time series
@@ -13,12 +13,12 @@ for i in range(1000):
  plt.axis('off')
  plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
  plt.margins(0, 0)
- plt.savefig("D:\Small Fishing Vessel Recognition\Original Data\Small Fishing\A%i.png"%(i+1))                     #Cycle to save MTF image
+ plt.savefig("D:\Small Fishing Vessel Recognition\Original Data\PE plastic vessels\A%i.png"%(i+1))                     #Cycle to save MTF image
  plt.show()
  i=i+1
  
 for i in range(1000):
- data=pd.read_csv("D:\Small Fishing Vessel Recognition\Original Data\UCR\A%i.csv"%(i+1),header=None) #Cyclically read data
+ data=pd.read_csv("D:\Small Fishing Vessel Recognition\Original Data\UCR Data\A%i.csv"%(i+1),header=None) #Cyclically read data
  X =data.values.tolist()                          # MTF transformation
  mtf = MarkovTransitionField(image_size=10)
  X_mtf = mtf.fit_transform(X)                     # Show the image for the first time series
@@ -28,6 +28,6 @@ for i in range(1000):
  plt.axis('off')
  plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
  plt.margins(0, 0)
- plt.savefig("D:\Small Fishing Vessel Recognition\Original Data\UCR\A%i.png"%(i+1))                     #Cycle to save MTF image
+ plt.savefig("D:\Small Fishing Vessel Recognition\UCR Data\A%i.png"%(i+1))                     #Cycle to save MTF image
  plt.show()
  i=i+1
