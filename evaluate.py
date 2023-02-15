ab4bf695b73a1340418af5ad317d72aa572ec75d
KunYuan888888 <109008605+KunYuan888888@users.noreply.github.com>
2023-02-15 20:33:45 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -8,6 +8,7 @@ train_num, valid_num, test_num= get_datasets()
 
 # Load the model
 new_model = tf.keras.models.load_model(config.model_dir)
+
 # Get the accuracy on the test set
 loss, acc = new_model.evaluate_generator(test_generator,
                                          steps=test_num // config.BATCH_SIZE)
